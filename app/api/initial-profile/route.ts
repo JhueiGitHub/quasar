06787c8f925e70c25a0471a7827292a0a930d23d
JhueiGitHub <64082345+JhueiGitHub@users.noreply.github.com>
2024-09-04 17:36:19 +0100

    api routes dynamic fix

--- app/api/initial-profile/route.ts
+++ app/api/initial-profile/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const user = await currentUser();
--- app/api/sidebar-data/route.ts
+++ app/api/sidebar-data/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const profile = await currentProfile();
